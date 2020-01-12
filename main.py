73299c2db98dae0aefe665661dd774b6b1e346df
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2020-01-11 16:04:01 -0800

    third iteration with improvement on detecting blue card background

--- main.py
+++ main.py
@@ -1,93 +1,199 @@
 import numpy as np
 from cv2 import cv2
 
+BACKGROUND_THRESHOLD = 20
+
 def main():
-    img = cv2.imread('training_images/ktp-1.png')
-    # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  img = cv2.imread('training_images/ktp-2.png')
 
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    h, s, v = cv2.split(hsv)
-    print(v[2][4])
-    value = 130 - v[0][0]
-    if value > 0:
-        hsv[:, :, 2] += value
-        img = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-        cv2.imwrite('img1.png', img)
-    else:
-        cv2.imwrite('img1.png', img)
+  prepreprocessed_img = prepreprocess_image(img)
+  cv2.imwrite('sample0.png', prepreprocessed_img)
 
-    # blur
-    b_im = cv2.GaussianBlur(img, (5, 5), 0)
-    cv2.imwrite('img2.png', b_im)
+  preprocessed_img = preprocess_image(prepreprocessed_img)
+  cv2.imwrite('sample1.png', preprocessed_img)
 
-    # to grayscale
-    gray_im = cv2.cvtColor(b_im, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite('img3.png', gray_im)
+  contours_sorted, contour_is_card = find_cards(preprocessed_img)
 
-    # threshold
-    _, thres_im = cv2.threshold(gray_im, 115, 255, cv2.THRESH_BINARY)
-    cv2.imwrite('img4.png', thres_im)
+  card_contours = []
+  for i in range(len(contours_sorted)):
+    if contour_is_card[i] == 1:
+      cv2.drawContours(img, [contours_sorted[i]], -1, (255, 0, 0), 2)
+  cv2.imwrite('sample2.png', img)
 
-    # dilation and erosion
-    de_im = cv2.erode(thres_im, None, iterations=10)
-    de_im = cv2.dilate(de_im, None, iterations=10)
-    cv2.imwrite('img5.png', de_im)
+  # if len(contours_sorted) != 0:
+  #   cards = []
 
-    # ims = gray_im.filter(ImageFilter.SHARPEN)
+  #   for i in range(len(contours_sorted)):
+  #     if contour_is_card[i] == 1:
+  #       draw_results(img, preprocess_card(contours_sorted[i], img))
+  # else:
+  #   print('Contours not found!')
 
-    # cv2.imwrite('tmp/img2.png', ims)
+def prepreprocess_image(img):
+  """
+  Preprocess the image to only show blue in the image.
+  We do this because KTP has a blue background
+  """
 
-def main2():
-    img = cv2.imread('training_images/ktp-1.png')
+  hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+  lower_blue = np.array([12, 12, 135])
+  upper_blue = np.array([168, 168, 255])
 
-    # convert to grayscale
-    grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
-    cv2.imwrite('img0.png', grayscale)
+  res = cv2.bitwise_and(img, img, mask = mask)
 
-    # # compute the Scharr gradient magnitude representation of the images
-    # # in both the x and y direction
-    # ddepth = cv2.CV_32F
-    # gradX = cv2.Sobel(grayscale, ddepth=ddepth, dx=1, dy=0, ksize=-1)
-    # gradY = cv2.Sobel(grayscale, ddepth=ddepth, dx=0, dy=1, ksize=-1)
+  return res
 
-    # # subtract the y-gradient from the x-gradient
-    # gradient = cv2.subtract(gradX, gradY)
-    # gradient = cv2.convertScaleAbs(gradient)
+def preprocess_image(img):
+  """Grays, blurs, and thresholds the img"""
 
-    # # blur and threshold the image
-    # blurred = cv2.blur(gradient, (9, 9))
-    # (_, thresh) = cv2.threshold(blurred, 225, 255, cv2.THRESH_BINARY)
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  blur = cv2.GaussianBlur(gray, (5, 5), 0)
 
-    # construct a closing kernel and apply it to the thresholded image
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (27, 9))
-    closed = cv2.morphologyEx(grayscale, cv2.MORPH_CLOSE, kernel)
+  """
+  The best threshold level depends on the ambient lighting conditions.
+  For bright lighting, a high threshold must be used to isolate the cards
+  from the background. For dim lighting, a low threshold must be used.
+  To make the card detector independent of lighting conditions, the
+  following adaptive threshold method is used.
 
-    cv2.imwrite('img1.png', closed)
+  A background pixel in the center top of the image is sampled to determine
+  its intensity. The adaptive threshold is set at THRESHOLD_ADDER higher
+  than that. This allows the threshold to adapt to the lighting conditions
+  """
 
-    # perform a series of erosions and dilations
-    eroded = cv2.erode(closed, None, iterations = 10)
-    dilated = cv2.dilate(eroded, None, iterations = 10)
+  img_w, img_h = np.shape(img)[:2]
+  background_level = gray[int(img_h/100)][int(img_w/2)] # 1% from top, 50% from side
+  threshold_level = background_level + BACKGROUND_THRESHOLD
 
-    cv2.imwrite('img2.png', dilated)
+  _, threshold = cv2.threshold(blur, threshold_level, 255, cv2.THRESH_BINARY)
 
-    # find contours
-    contours = cv2.findContours(
-      dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
-    )
-    contours = contours[1]
+  return threshold
 
-    print(contours)
-    for x in range(len(contours)):
-        contour = contours[x]
+# generates a flattened image of the card
+# returns contours, and boolean to indicate if the contour is a card
+def find_cards(img):
+  """
+  Finds all card-sized contours in thresholded image. Returns the number
+  of cards, and a list of card controus sorted from largest to smallest.
+  """
 
-        # compute the rotated bounding box of the largest contour
-        rect = cv2.minAreaRect(contour)
-        box = cv2.cv.BoxPoints(rect) if imutils.is_cv2() else cv2.boxPoints(rect)
-        box = numpy.int0(box)
+  # Find contours and sort their indices by contour size
+  contours, hierarchies = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+  index_sort = sorted(range(len(contours)), key=lambda i : cv2.contourArea(contours[i]), reverse=True)
 
-        # draw a bounding box arounded the detected barcode and display the image
-        cv2.drawContours(img, [box], -1, (255, 0, 0), 3)
+  # If there are no contours, do nothing
+  if len(contours) == 0:
+    return [], []
+
+  # Otherwise, initialize empty sorted contour and hierarchy lists
+  contours_sort = []
+  hierarchies_sort = []
+  contour_is_card = np.zeros(len(contours), dtype=int)
+
+  # hierarchy array can be used to check if the contours have parents or not
+  for i in index_sort:
+    contours_sort.append(contours[i])
+    hierarchies_sort.append(hierarchies[0][i])
+
+  """
+  Determine which of the contours are cards by applying the following criteria
+  1. Smaller area than the maximum card size
+  2. Bigger area than the minimum card size
+  3. Have no parents
+  4. Have four corners
+  """
+  for i in range(len(contours_sort)):
+    size = cv2.contourArea(contours_sort[i])
+    perimeter = cv2.arcLength(contours_sort[i], True)
+    approx = cv2.approxPolyDP(contours_sort[i], 0.01*perimeter, True)
+
+    if hierarchies_sort[i][3] == -1 and len(approx) == 4:
+      contour_is_card[i] = 1
+
+  return contours_sort, contour_is_card
+
+def preprocess_card(img, contour):
+  """
+  Uses contour to find information about the card. 
+  Returns flattened, re-sized, grayed image.
+  Ref: http://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-tranform-example/
+  """
+
+  perimeter = cv2.arcLength(contour, True)
+  approx = cv2.approxPolyDP(contour, 0.01 * perimeter, True)
+  points = np.float32(approx)
+
+  x, y, w, h = cv2.boundingRect(contour)
+
+  temp_rect = np.zeros((4, 2), dtype='float32')
+
+  s = np.sum(points, axis = 2)
+
+  tl = points[np.argmin(s)] 
+  br = points[np.argmax(s)]
+
+  diff = np.diff(points, axis = -1)
+  
+  tr = points[np.argmin(diff)]
+  bl = points[np.argmax(diff)]
+
+  # Need to create an array listing points in order of
+  # [top left, top right, bottom right, bottom left]
+  # before doing the perspective transform
+
+  if w <= 0.8*h: # If card is vertically oriented
+    temp_rect[0] = tl
+    temp_rect[1] = tr
+    temp_rect[2] = br
+    temp_rect[3] = bl
+
+  if w >= 1.2*h: # If card is horizontally oriented
+    temp_rect[0] = bl
+    temp_rect[1] = tl
+    temp_rect[2] = tr
+    temp_rect[3] = br
+
+  # If the card is 'diamond' oriented, a different algorithm
+  # has to be used to identify which point is top left, top right
+  # bottom left, and bottom right.
+
+  if w > 0.8*h and w < 1.2*h: #If card is diamond oriented
+    # If furthest left point is higher than furthest right point,
+    # card is tilted to the left.
+    if pts[1][0][1] <= pts[3][0][1]:
+      # If card is titled to the left, approxPolyDP returns points
+      # in this order: top right, top left, bottom left, bottom right
+      temp_rect[0] = pts[1][0] # Top left
+      temp_rect[1] = pts[0][0] # Top right
+      temp_rect[2] = pts[3][0] # Bottom right
+      temp_rect[3] = pts[2][0] # Bottom left
+
+      # If furthest left point is lower than furthest right point,
+      # card is tilted to the right
+      if pts[1][0][1] > pts[3][0][1]:
+        # If card is titled to the right, approxPolyDP returns points
+        # in this order: top left, bottom left, bottom right, top right
+        temp_rect[0] = pts[0][0] # Top left
+        temp_rect[1] = pts[3][0] # Top right
+        temp_rect[2] = pts[2][0] # Bottom right
+        temp_rect[3] = pts[1][0] # Bottom left
+
+  maxWidth = 200
+  maxHeight = 300
+
+  # Create destination array, calculate perspective transform matrix,
+  # and warp card image
+  dst = np.array([[0,0],[maxWidth-1,0],[maxWidth-1,maxHeight-1],[0, maxHeight-1]], np.float32)
+  M = cv2.getPerspectiveTransform(temp_rect,dst)
+  warp = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+  warp = cv2.cvtColor(warp,cv2.COLOR_BGR2GRAY)
+
+  return warp
+
+def draw_result(img, card):
+  return None
 
 if __name__ == '__main__':
-  main2()
+  main()
