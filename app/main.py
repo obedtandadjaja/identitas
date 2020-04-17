b537cf14bb4b32dd2212f037c2ccb3e18484e7ed
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2020-04-16 23:08:44 -0700

    restructure routing

--- app/main.py
+++ app/main.py
@@ -1,22 +1,35 @@
 from fastapi import Depends, FastAPI, Header, HTTPException
+from starlette.middleware.cors import CORSMiddleware
+from starlette.requests import Request
 
-from app.api.v1.endpoints import auth, ktp
+from app.api.v1.api import api_router as api_v1_router
+from app.db.session import Session
 
 
-app = FastAPI()
+app = FastAPI(title="Identitas")
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # allow everything for now. Change later
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.middleware("http")
+async def db_session_middleware(request: Request, call_next):
+    request.state.db = Session()
+    response = await call_next(request)
+    request.state.db.close()
+    return response
+
+# async def get_token_header(authorization: str = Header(...)):
+#     if authorization != "secret":
+#         raise HTTPException(status_code=400, detail="Authorization header invalid")
+
+app.include_router(api_v1_router, prefix="/api/v1/")
 
 @app.get("/health")
 def read_health():
     return {"status": "OK"}
-
-async def get_token_header(authorization: str = Header(...)):
-    if authorization != "secret":
-        raise HTTPException(status_code=400, detail="Authorization header invalid")
-
-app.include_router(auth.router)
-app.include_router(
-        ktp.router,
-        prefix="/api/v1/ktp",
-        dependencies=[Depends(get_token_header)],
-        responses={404: {"description": "Not Found"}},
-)
